74bd434b8be3d253cddde025e8ec4a8ebbf2f966
Christian Mischok <christianmischok@Christians-MBP.fritz.box>
2017-11-08 16:18:06 +0100

    WIP: added form in create ico

--- src/pages/GenerateICO/Name/index.js
+++ src/pages/GenerateICO/Name/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Name = () => (
+  <div>
     <h1>Name</h1>
+    <form id="name-form">
+      <label>How shall your Token be named?</label>
+      <br/>
+      <input type="text" name="token-name"/>
+      <label>What would be your ticker symbol?</label>
+      <br/>
+      <input type="text" name="ticker-name"/>
+    </form>
+  </div>
+
 )
 
 export default Name;
