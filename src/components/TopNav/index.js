8dc05f1b7d9e6d0282dd1953e4749de5ce2cafa9
Michael Franz Ettlinger <ettlinger@campus.tu-berlin.de>
2017-11-09 20:27:00 +0100

    Added current account to Top Navigation

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Menu, Segment } from 'semantic-ui-react';
+import { Menu, Segment, Button } from 'semantic-ui-react';
+
+import { PropTypes } from 'prop-types';
 
 class TopNav extends Component {
 
-    state = {}
+    state = {
+    }
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
@@ -15,11 +18,11 @@ class TopNav extends Component {
         return (
             <Segment color='teal' size='small' inverted>
                 <Menu size='large' color='teal' inverted secondary borderless>
-                    <Menu.Item header style={{ fontSize: '18px' }}>GENTO</Menu.Item>
+                    <Menu.Item name='home' to='/' active={activeItem === 'home'} as={ Link }  onClick={this.handleItemClick} header style={{ fontSize: '18px' }}>GENTO</Menu.Item>
                     <Menu.Menu position='right'>
-                        <Menu.Item name='about' as={ Link } to='/' active={activeItem === 'about'} onClick={this.handleItemClick} />
                         <Menu.Item name='generate' as={ Link } to='/generate' active={activeItem === 'generate'} onClick={this.handleItemClick} />
                         <Menu.Item name='list' as={ Link } to='/list' active={activeItem === 'list'} onClick={this.handleItemClick} />
+                        <Menu.Item><Button inverted color="teal">{this.props.account}</Button></Menu.Item>
                     </Menu.Menu>                                
                 </Menu>
             </Segment>
@@ -27,6 +30,10 @@ class TopNav extends Component {
     }
 }
 
+TopNav.propTypes = {
+    account: PropTypes.string.isRequired,
+    notify: PropTypes.func.isRequired
+}
 export default TopNav;
 
 
