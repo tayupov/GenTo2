20fd2778593444d46454a2bc1d56a38aca18b665
TheoVitkovskiy <fvitkovski@mail.de>
2017-11-10 07:31:35 +0100

    Change the order of the NavBar menu items

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -21,9 +21,9 @@ class TopNav extends Component {
                     <Menu size='large' color='teal' inverted secondary borderless>
                         <Menu.Item name='home' to='/' active={activeItem === 'home'} as={ Link }  onClick={this.handleItemClick} header style={{ fontSize: '18px' }}>GENTO</Menu.Item>
                         <Menu.Menu position='right'>
+                            <Menu.Item><Button inverted color="teal">{this.props.account}</Button></Menu.Item>
                             <Menu.Item name='generate' as={ Link } to='/generate' active={activeItem === 'generate'} onClick={this.handleItemClick} />
                             <Menu.Item name='list' as={ Link } to='/list' active={activeItem === 'list'} onClick={this.handleItemClick} />
-                            <Menu.Item><Button inverted color="teal">{this.props.account}</Button></Menu.Item>
                         </Menu.Menu>
                     </Menu>
                 </Container>
