1d283a2fc98dfb918407e6d51274bcf24bb150c3
Christian Mischok <christianmischok@Christians-MBP.fritz.box>
2017-11-17 21:53:41 +0100

    added dropdown menu in navigation bar for generate and poll

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -11,6 +11,14 @@ class TopNav extends Component {
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
+    hideAccountHash = () => {
+      var accountHash = this.props.account;
+      console.log('str: ' + accountHash);
+      var firstTenSigns = accountHash.replace(accountHash.substring(10,accountHash.length-1), '*');
+      return firstTenSigns;
+
+    };
+
     render() {
         const { activeItem } = this.state;
 
@@ -20,10 +28,28 @@ class TopNav extends Component {
                     <Menu size='large' color='teal' inverted secondary borderless>
                         <Menu.Item name='home' to='/' active={activeItem === 'home'} as={ Link }  onClick={this.handleItemClick} header style={{ fontSize: '18px' }}>GENTO</Menu.Item>
                         <Menu.Menu position='right'>
-                            <Menu.Item><Button inverted color="teal">{this.props.account}</Button></Menu.Item>
-                            <Menu.Item name='generate' as={ Link } to='/generate' active={activeItem === 'generate'} onClick={this.handleItemClick} />
-                            <Menu.Item name='poll' as={ Link } to='/poll' active={activeItem === 'poll'} onClick={this.handleItemClick} />                            
+                            <Menu.Item name='generate' class='ui compact menu' as={ Link } to='/generate' active={activeItem === 'generate'} onClick={this.handleItemClick} >
+                              <div class='ui simple dropdown item'>Generate <i class='dropdown icon'></i>
+                                <div class='menu'>
+                                  <div class='item'>Create ICO</div>
+                                  <div class='item'>Create DAO</div>
+                                </div>
+                              </div>
+                            </Menu.Item>
+                            <Menu.Item name='poll'  class='ui compact menu' as={ Link } to='/poll' active={activeItem === 'poll'} onClick={this.handleItemClick} >
+                              <div class='ui simple dropdown item'>Poll <i class='dropdown icon'></i>
+                                <div class='menu'>
+                                  <div class='item'>Create a poll</div>
+                                  <div class='item'>Create a vote</div>
+                                </div>
+                              </div>
+                            </Menu.Item>
                             <Menu.Item name='list' as={ Link } to='/list' active={activeItem === 'list'} onClick={this.handleItemClick} />
+                            {console.log(this.props.account)}
+                            <Menu.Item>
+                              Account:
+                            </Menu.Item>
+                            <Menu.Item><Button inverted color='teal' /*style={{color: red}}*/>{this.props.account}</Button></Menu.Item>
                         </Menu.Menu>
                     </Menu>
                 </Container>
