47d6ce01cb0da35a44d95329d3bf49565cccd30e
Christian M <mischok.christian@gmail.com>
2017-11-19 09:00:54 +0100

    [WIP] insert modal for ico creating

--- src/pages/GenerateICO/GenerateICO.js
+++ src/pages/GenerateICO/GenerateICO.js
@@ -15,44 +15,54 @@ import HeaderSection from 'components/Header';
 
 import './main.css';
 
-const GenerateICO = ({
+/*const GenerateICO = ({
     handleShow, handleHide, active, steps
-}) => (
+}) => (*/
 
-/*class GenerateICO extends Component {*/
-
-
+class GenerateICO extends Component {
 
 
+  constructor(props) {
+    super(props);
+
+
+    this.state = {
+      steps: this.props.steps,
+      handleShow: this.props.handleShow,
+      handleHide: this.pops.handleHide,
+      active: this.props.active,
+      open: false
+    }
+  }
+
+  state = {}
+
+
+  closeConfigShow = (closeOnRootNodeClick) => () => {
+    this.setState({closeOnRootNodeClick, open: true})
+  }
+
+
+  //close = () => this.setState(open: false);
 
   render() {
 
-    state = {open: false}
-
-
+    const { open, closeOnRootNodeClick } = this.state
 
     return(
 
-
       <div>
-          {closeConfigShow = (closeOnRootNodeClick) => () => {
-            this.setState({closeOnRootNodeClick, open: true})
-          }
-
-          close = () => this.setState(open: false);
-
-          const { open, closeOnRootNodeClick } = this.state}
           <HeaderSection text="GENERATE YOUR OWN SMART CONTRACTS" />
-          <Button onClick={this.closeOnRootNodeClick(true, false)} color='teal'>Create an ICO</Button>
+          <Button onClick={this.state.handleShow} color='teal'>Create an ICO</Button>
           <Dimmer
               page
-              active={active}
+              active={this.state.active}
           >
               <Container
                   style={{ width: '700px', position: 'relative' }}
               >
                   <Button
-                      onClick={handleHide}
+                      onClick={this.state.handleHide}
                       icon='close'
                       floated='right'
                       basic
@@ -62,11 +72,17 @@ const GenerateICO = ({
                       className='step-progress'
                       style={{ width: '600px', marginLeft: '3.3em' }}
                   >
-                    <Modal open={open} closeOnRootNodeClick={closeOnRootNodeClick} onClose={this.close}>
-                      <Modal.Header>Create an ICO</Modal.Header>
+                    <Modal open={this.state.open}
+                           closeOnRootNodeClick={closeOnRootNodeClick}
+                           onClose={this.close}
+                    >
+
+                      <Modal.Header>
+                        Create an ICO
+                      </Modal.Header>
                       <Modal.Content>
                         <StepZilla
-                            steps={steps}
+                            steps={this.state.steps}
                             nextButtonCls="ui positive button"
                             backButtonCls="ui positive button"
                         />
@@ -78,7 +94,7 @@ const GenerateICO = ({
       </div>
     )
   }
-)
-//}
+//)
+}
 
 export default GenerateICO;
