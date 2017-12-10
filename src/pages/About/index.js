d3e62e98d3821f306e30ae70ef9169964ced4460
TheoVitkovskiy <fvitkovski@mail.de>
2017-12-10 17:59:57 +0100

    Demo (WIP)

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -5,19 +5,19 @@ import Header from 'components/Header';
 const About = () => (
     <div>
         <Header text='ABOUT GENTO' id='header' />
-        <p style={{ fontSize: '20px' }}>
-                GenTo is a platform that facilitates Initial Coin Offerings.
+        <p style={{ fontSize: '20px', textAlign: 'center' }}>
+                GenTo is a platform that facilitates Initial Coin Offerings and creation/governance of DAOs.
                 It creates user-defined
                 Smart Contracts and automatically deploys them to the blockchain - without the need
                 to write a single line of code!
             <br></br>
             <br></br>
-                Hiding technical details from the user, we make Initial Coin Offerings accessible for
+                Hiding technical details from the user, we make DAOs accessible for
                 a broader, non-technical audience. GenTo provides an intuitive web-based UI that
                 guides the user through the contract creation process step by step.
             <br></br>
             <br></br>
-                GenTo has been developed by a student group at the chair for Information Systems
+                GenTo2 has been developed by a student group at the chair for Information Systems
                 Engineering at Technical University of Berlin in collaboration with venture
                 capitalist and startup advisor Globumbus.
             <br></br>
