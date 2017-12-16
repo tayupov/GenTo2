05220ac08baa705598f3cbdf1c6e1637d6a92cf6
TheoVitkovskiy <fvitkovski@mail.de>
2017-12-16 08:09:54 +0100

    Add one more test

--- solidity/test/AuctionToken.test.js
+++ solidity/test/AuctionToken.test.js
@@ -12,9 +12,6 @@ async function getTestToken() {
 
 contract('AuctionToken', function(accounts) {
 
-  const buyerAddress = accounts[0];
-  const ownerAddress = accounts[1];
-
   it("should be possible to mock the time in the test contract", async function() {
     const testContract = await getTestToken()
     //The time of the mock contract is set to 0 in the beginning for convenience reasons
@@ -25,7 +22,8 @@ contract('AuctionToken', function(accounts) {
     //It should also be possible to go back in time!
     await testContract.setCurrentTime.sendTransaction(0)
     expect(+await testContract.currentTime.call()).toBe(0) //truffle returns strings for numbers
-  });
+  })
+
   it("should not be possible to compute a buyPrice when the ICO is not running", async function() {
     const testContract = await getTestToken()
 
@@ -34,6 +32,7 @@ contract('AuctionToken', function(accounts) {
     await testContract.setCurrentTime.sendTransaction(2000000)
     await expect(testContract.getBuyPrice.call()).rejects.toEqual(expect.any(Error))
   })
+
   it("should return a linearly increasing price during the ICO with getBuyPrice()", async function() {
     const testContract = await getTestToken()
     await testContract.setCurrentTime.sendTransaction(1000000)
@@ -48,13 +47,14 @@ contract('AuctionToken', function(accounts) {
       expect(+await testContract.getBuyPrice.call()).toBeCloseTo(currentPrice, 5)
     }
   })
+
   it("should be possible to buy something during the ICO", async function() {
     const testContract = await getTestToken()
-    // const initialBalance = web3.eth.getBalance(web3.eth.accounts[1])
     await testContract.setCurrentTime.sendTransaction(1200000)
     await testContract.getBuyPrice.call()
     await testContract.buy.sendTransaction({from: accounts[1], value: web3.toWei(10, 'Gwei')});
   })
+
   it("should not be possible to buy something outside the ICO", async function() {
     const testContract = await getTestToken()  
     try {
@@ -65,20 +65,19 @@ contract('AuctionToken', function(accounts) {
       expect(e.message).toContain("VM Exception while processing transaction: ")
     }
   })
+
   it("should calculate the right bought amount with buy()", async function() {
     const testContract = await getTestToken()
     await testContract.setCurrentTime.sendTransaction(1200000)
-    // console.log(web3.eth.getBalance(web3.eth.accounts[1]).toNumber())
-    // await testContract.buy.sendTransaction({from: accounts[1], value: 10000})
     const boughtAmount = await testContract.buy.call({from: accounts[1], value: 10000})
     const buyPrice = await testContract.getBuyPrice.call()
     const expectedAmount = 10000 / buyPrice.toNumber()
     expect(boughtAmount.toNumber()).toBeCloseTo(expectedAmount, 0);
   })
+
   it("should not be possible to buy something with amount < buyPrice", async function() {
     const testContract = await getTestToken()
     await testContract.setCurrentTime.sendTransaction(1200000)
-    console.log(+await testContract.getBuyPrice.call())
     let buyPrice = await testContract.getBuyPrice.call()
     const smallerBuyPrice = --buyPrice;
     try {
@@ -88,4 +87,20 @@ contract('AuctionToken', function(accounts) {
       expect(e.message).toContain("VM Exception while processing transaction: ")
     }
   })
+
+  it("should not be possible to buy more tokens than there are in the ICO", async function() {
+    const testContract = await getTestToken()
+    await testContract.setCurrentTime.sendTransaction(1200000)
+    const currTokens = await testContract.balanceOf(accounts[0])
+    const buyPrice = await testContract.getBuyPrice.call()
+    let buyValue = currTokens * buyPrice
+    const actBuyValue = buyValue += buyPrice
+    try {
+      await testContract.buy.sendTransaction({from: accounts[2], value: actBuyValue})
+      should.fail("this transaction should have raised an error")
+    } catch (e) {
+      expect(e.message).toContain("VM Exception while processing transaction: ")
+    }
+  })
+
 });
