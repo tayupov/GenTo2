bd75d3b3ffa5b137dba95773e7fa8909aa59b106
Christian M <mischok.christian@gmail.com>
2018-01-09 12:12:03 +0100

    changed naming from VotingToken.test.js to Proposals.test.js and changed to Proposals.sol in migrations

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 const tokenFactory = artifacts.require("./GentoDaoFactory.sol");
 const AuctionToken = artifacts.require("./GentoDao.sol");
-const VotingToken = artifacts.require("./VotingToken.sol");
+const VotingToken = artifacts.require("./Proposals.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(tokenFactory);
