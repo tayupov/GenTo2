1dc8092d8de238fbd745f45219c786ad1b824875
TheoVitkovskiy <fvitkovski@mail.de>
2018-01-22 13:54:36 +0100

    Resolved conflict in 2_deploy_contracts.js

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 const tokenFactory = artifacts.require("./GentoDaoFactory.sol");
 const DAO = artifacts.require("./GentoDao.sol");
-=======
-const DAOFactory = artifacts.require("./GentoDaoFactory.sol");
->>>>>>> refactored contracts
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(tokenFactory);
