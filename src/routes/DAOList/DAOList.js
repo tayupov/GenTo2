590477ac4a1e558384fc537914e1df1e7e1f183f
Christian <christian@Christians-MacBook-Pro-2.local>
2018-01-24 17:41:59 +0100

    split up DAOs and ICOs in list

--- src/routes/DAOList/DAOList.js
+++ src/routes/DAOList/DAOList.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card, Divider } from 'semantic-ui-react';
+import { Card, Divider, Tab } from 'semantic-ui-react';
 
 export default class DAOList extends React.Component {
   render() {
-    console.log(this.props)
-    const { gentoOrganizations } = this.props
+    const { gentoDAOs, gentoICOs } = this.props
+    const panes = [
+      { menuItem: 'ICOs', render: () => <Tab.Pane>{renderTabPane(gentoICOs)}</Tab.Pane> },
+      { menuItem: 'DAOs', render: () => <Tab.Pane>{renderTabPane(gentoDAOs)}</Tab.Pane> }
+    ]
+
     return (
       <div>
-      <h1>All GenTo DAOs</h1>
+      <h1>All GenTo ICOs and DAOs</h1>
       <Divider section hidden />
-      <Card.Group>
-        {gentoOrganizations.map((org, index) => {
-          return (
-            <Card as={Link} to={{ pathname: `/dao/${org.address}` }} key={index}>
-                <Card.Content>
-                  <Card.Header> {org.name} </Card.Header>
-                  <Card.Meta> {org.symbol} </Card.Meta>
-                </Card.Content>
-                <Card.Content description = {org.isICOFinished} />
-                <Card.Content description = {"Number Of Proposals: " + org.numberOfProposals.c} />
-                <Card.Content description = {"Sale from " + org.saleStart + " to " + org.saleEnd} />
-            </Card>
-          )
-        })}
-      </Card.Group>
+      <Tab panes= {panes} />
       </div>
     )
   }
+}
+
+function renderTabPane(gentoOrganizations) {
+  return (
+    <Card.Group>
+      {gentoOrganizations.map((org, index) => {
+        return (
+          <Card as={Link} to={{ pathname: `/dao/${org.address}` }} key={index}>
+              <Card.Content>
+                <Card.Header> {org.name} </Card.Header>
+                <Card.Meta> {org.symbol} </Card.Meta>
+              </Card.Content>
+              <Card.Content description = {"Number Of Proposals: " + org.numberOfProposals.c} />
+              <Card.Content description = {"Sale from " + org.saleStart + " to " + org.saleEnd} />
+          </Card>
+        )
+      })}
+    </Card.Group>
+  )
 }
--- src/routes/DAOList/DAOListContainer.js
+++ src/routes/DAOList/DAOListContainer.js
@@ -8,22 +8,23 @@ export default class DAOListContainer extends Component {
   constructor() {
     moment().format();
     super();
-    this.state = { gentoOrganizations: [] }
+    this.state = { gentoDAOs: [], gentoICOs: [] }
   }
 
   async componentDidMount() {
     let gentoOrganizations = await loadAllOrganizations();
     gentoOrganizations.forEach(org => {
-      org.isICOFinished ? org.isICOFinished = "ICO is running" : org.isICOFinished = "ICO is finished";
       org.saleStart = moment.unix(org.saleStart.c).format("DD.MM.YYYY");
       org.saleEnd = moment.unix(org.saleEnd.c).format("DD.MM.YYYY");
     })
-    this.setState({ gentoOrganizations })
+    const gentoDAOs = gentoOrganizations.filter(org => org.isICOFinished);
+    const gentoICOs = gentoOrganizations.filter(org => !org.isICOFinished);
+    this.setState({ gentoDAOs, gentoICOs })
   }
 
   render() {
     return (
-      <DAOList gentoOrganizations={this.state.gentoOrganizations} />
+      <DAOList gentoDAOs={this.state.gentoDAOs} gentoICOs={this.state.gentoICOs} />
     );
   }
 }
