b6fab3a1def883ce36e5092ae6bd0423ec7b7630
Christian <christian@Christians-MacBook-Pro-2.local>
2018-01-24 17:02:28 +0100

    fix dao list

--- src/routes/DAOList/DAOList.js
+++ src/routes/DAOList/DAOList.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card } from 'semantic-ui-react';
+import { Card, Divider } from 'semantic-ui-react';
 
 export default class DAOList extends React.Component {
   render() {
     console.log(this.props)
     const { gentoOrganizations } = this.props
     return (
+      <div>
+      <h1>All GenTo DAOs</h1>
+      <Divider section hidden />
       <Card.Group>
         {gentoOrganizations.map((org, index) => {
           return (
             <Card as={Link} to={{ pathname: `/dao/${org.address}` }} key={index}>
-              <Card.Content>
-                <Card.Header>
-                  {org.name} <h5>{org.symbol}</h5>
-                </Card.Header>
-                <Card.Description>
-                  ICO finished {org.isICOFinished}<br/>
-                  Number Of Proposals {org.numberOfProposals.c}<br/>
-                  Sale Start {org.saleStart.c}<br/>
-                  Sale End {org.saleEnd.c}<br/>
-                </Card.Description>
-              </Card.Content>
+                <Card.Content>
+                  <Card.Header> {org.name} </Card.Header>
+                  <Card.Meta> {org.symbol} </Card.Meta>
+                </Card.Content>
+                <Card.Content description = {org.isICOFinished} />
+                <Card.Content description = {"Number Of Proposals: " + org.numberOfProposals.c} />
+                <Card.Content description = {"Sale from " + org.saleStart + " to " + org.saleEnd} />
             </Card>
           )
         })}
       </Card.Group>
+      </div>
     )
   }
 }
--- src/routes/DAOList/DAOListContainer.js
+++ src/routes/DAOList/DAOListContainer.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-
+import moment from 'moment';
 import DAOList from './DAOList';
 import { loadAllOrganizations } from 'provider/DAOListProvider';
 
 export default class DAOListContainer extends Component {
 
   constructor() {
+    moment().format();
     super();
     this.state = { gentoOrganizations: [] }
   }
 
   async componentDidMount() {
-    const gentoOrganizations = await loadAllOrganizations()
+    let gentoOrganizations = await loadAllOrganizations();
+    gentoOrganizations.forEach(org => {
+      org.isICOFinished ? org.isICOFinished = "ICO is running" : org.isICOFinished = "ICO is finished";
+      org.saleStart = moment.unix(org.saleStart.c).format("DD.MM.YYYY");
+      org.saleEnd = moment.unix(org.saleEnd.c).format("DD.MM.YYYY");
+    })
     this.setState({ gentoOrganizations })
   }
 
