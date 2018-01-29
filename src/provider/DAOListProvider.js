c81ff6d376e26ae2e550522b532184b92b9e24d4
roman <roman@weissmaler.de>
2018-01-29 21:47:17 +0100

    Update DAOListProvider.js

--- src/provider/DAOListProvider.js
+++ src/provider/DAOListProvider.js
@@ -17,8 +17,6 @@ const mapOrganization = async (organization) => {
     numberOfShareholders: await organization.getShareholderCount(),
     totalSupply: await organization.totalSupply(),
     remainingTokensForICOPurchase: await organization.remainingTokensForICOPurchase()
- // buyPrice: await organization.getBuyPrice() 
- // does not work for all organizations, we need some error handling here or leave it out
   }
 }
 
@@ -47,16 +45,9 @@ export async function loadAllOrganizations() {
   return Promise.all(organizations.map(mapOrganization))
 }
 
-<<<<<<< 37436535dd234a8f9f81c8cb57bd7dbbf3d72daf
 export async function loadAllOrganizationsOfOwner(owner) {
   if (!owner) return []
   const organizations = await prepareOrganizations()
   const organizationsWithOwnership = await filterByBalance(organizations, owner)
   return Promise.all(organizationsWithOwnership.map(mapOrganization))
-=======
-    const gentoDAOAddresses = await gentoDAOFactory.getDAOs.call()
-    const organizations = await Promise.all(gentoDAOAddresses.map(address => loadOrganization(address)))
-    const organizationsWithOwnership = await filterByBalance(organizations, owner)
-    return Promise.all(organizationsWithOwnership.map(mapOrganization));
->>>>>>> I cant get git to work
 }
