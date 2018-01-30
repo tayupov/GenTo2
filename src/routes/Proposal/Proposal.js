ccf73d6c4c92a70a33f07598fde8b8a47be1196a
Christian <christian@Christians-MacBook-Pro-2.local>
2018-01-30 18:05:37 +0100

    display information on details view

--- src/routes/Proposal/Proposal.js
+++ src/routes/Proposal/Proposal.js
@@ -1,37 +1,88 @@
 import React from 'react';
-
-import { Link } from 'react-router-dom';
-import { Button} from 'semantic-ui-react';
+import { Button, Divider, Table, Message, Label } from 'semantic-ui-react';
 
 import { loadProposal } from 'provider/ProposalProvider'
 
 export default class Proposal extends React.Component {
+  render() {
+    const { proposal, vote } = this.props;
+    const { approveCallback, disapproveCallback,executeCallback}= this.props;
+    return (
+      <div>
+        <h1>Information on proposal: {proposal.name}</h1>
+        <Divider section hidden />
+        <Button onClick={approveCallback} content="Approve" />
+        <Button onClick={disapproveCallback} content="Disapprove" />
+        <Button onClick={executeCallback} content="Execute" />
+        <h3>{vote.stateDescription}</h3>
+        <Divider section hidden />
+        
+        <Message>
+          <Message.Header>Proposal Description:</Message.Header>
+          <p> {proposal.description} </p>
+        </Message>
 
-
-    constructor() {
-        super();
-    }
-
-    render() {
-        const { proposal, vote } = this.props;
-        const { approveCallback, disapproveCallback,executeCallback}= this.props;
-        return (
-        <div>
-            <h1>{proposal.name}</h1>
-            <h2>description: {proposal.description}</h2>
-            <h2>amount: {proposal.amount}</h2>
-            <h2>recipient: {proposal.recipient}</h2>
-            <h2>proposalDeadline: {proposal.proposalDeadline}</h2>
-            <h2>finished: {proposal.finished}</h2>
-            <h2>proposalPassed: {proposal.proposalPassed}</h2>
-            <h2>passedPercent: {proposal.passedPercent}</h2>
-            <h2>fieldOfWork: {proposal.fieldOfWork}</h2>
-            <h2>dividend: {proposal.dividend}</h2>
-            <h2>this user voted: {String(vote.voted)}</h2>
-            <h2>support from this user: {String(vote.support)}</h2>
-            <Button  onClick={approveCallback} content="Approve" />
-            <Button  onClick={disapproveCallback} content="Disapprove" />
-            <Button  onClick={executeCallback} content="Execute" />
-        </div>)
-    }
+        <Table celled>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                <Label size='large'>Tokens transferred to Recipient when finished</Label>
+              </Table.Cell>
+              <Table.Cell>
+                <Label size='large'>{proposal.amount} Wei</Label>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Label size='large'>Recipient Address</Label>
+              </Table.Cell>
+              <Table.Cell>
+                <Label size='large'>{proposal.recipient}</Label>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Label size='large'>Deadline of Proposal</Label>
+              </Table.Cell>
+              <Table.Cell>
+                <Label size='large'>{proposal.proposalDeadline}</Label>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Label size='large'>Proposal State</Label>
+              </Table.Cell>
+              <Table.Cell>
+                <Label size='large'>{proposal.stateDescription}</Label>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Label size='large'>Voters currently approving the Proposal</Label>
+              </Table.Cell>
+              <Table.Cell>
+                <Label size='large'>{proposal.passedPercent}%</Label>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Label size='large'>Field of Work</Label>
+              </Table.Cell>
+              <Table.Cell>
+                <Label size='large'>{proposal.fieldOfWorkDescription}</Label>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Label size='large'>Dividend for Voting</Label>
+              </Table.Cell>
+              <Table.Cell>
+                <Label size='large'>{proposal.dividend} Wei</Label>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>  
+        </Table>
+      </div>
+    )
+  }
 }
