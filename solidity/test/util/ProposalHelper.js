86e1433a67ede24c4025b24a5ab5aeffbbd519c3
Christian M <mischok.christian@gmail.com>
2018-02-01 20:54:45 +0100

    added helper class for reusing code for creatingIco, voting, listen on events and for retrieving a formatted proposal, WIP: started to refactor the tests to use the new helper methods

---
+++ solidity/test/util/ProposalHelper.js
@@ -0,0 +1,78 @@
+const proposalFields = [
+   "recipient",
+   "amount",
+   "name",
+   "description",
+   "proposalDeadline",
+   "finished",
+   "proposalPassed",
+   "passedPercent",
+   "dividend",
+   "dmr"
+ ]
+
+// module.exports.simulateICO = async function(accObj, contract, accounts) {
+//   await contract.setCurrentTime.sendTransaction(1200000)
+//   Object.keys(accObj).forEach(async function(accKey) {
+//     await contract.buy.sendTransaction({from: accounts[accKey] , value: accObj[accKey]})
+//   });
+//   await contract.setCurrentTime.sendTransaction(2200000)
+// }
+//
+// module.exports=(dao) => {
+//   return async (proposalID) => {
+//     const proposalRawData = await dao.getProposal.call(proposalID)
+//     if (proposalRawData.length != proposalFields.length) throw "someone changed get proposal. You need to change this aswell or all tests will fail"
+//     const proposalFormatted = {}
+//     for (let i=0; i<proposalFields.length; i++) {
+//       proposalFormatted[proposalFields[i]] = proposalRawData[i]
+//     }
+//     return proposalFormatted
+//   }
+//
+// }
+
+module.exports = function(contract, accounts) {
+
+  async function simulateIco(accObj) {
+    // Set time between ICO start and END
+    await contract.setCurrentTime.sendTransaction(1200000)
+    Object.keys(accObj).forEach(async function(accKey) {
+      await contract.buy.sendTransaction({from: accounts[accKey] , value: accObj[accKey]})
+    });
+    // Set time to after ICO
+    await contract.setCurrentTime.sendTransaction(2200000)
+  }
+
+  async function getFormattedProposal(proposalID) {
+    const proposalRawData = await contract.getProposal.call(proposalID)
+    if (proposalRawData.length != proposalFields.length) throw new Error("The proposal doesn't have the correct format. Please check the properties")
+    const proposalFormatted = {}
+    for (let i=0; i<proposalFields.length; i++) {
+      proposalFormatted[proposalFields[i]] = proposalRawData[i]
+    }
+    return proposalFormatted
+  }
+
+  async function listenForEvent(eventName) {
+    // get the event listener for the specific event
+    const listener = contract[eventName]();
+    const log = await new Promise((resolve, reject) => listener.get(
+        (error, log) => error ? reject(error) : resolve(log)));
+    // check that there one new log object
+    assert.equal(log.length, 1, 'should be one new event log object');
+    // return only the properties which are important for testing
+    return log[0].args;
+  }
+
+  async function voteBulk(proposalID, accObj) {
+    Object.keys(accObj).forEach(async function(accKey) {
+      await contract.vote.sendTransaction(proposalID, accObj[accKey], {from: accounts[accKey]})
+    })
+    // Set time to after the Proposal period
+    await contract.setCurrentTime.sendTransaction(2300000)
+  }
+
+  return {simulateIco: simulateIco, getFormattedProposal: getFormattedProposal,
+    listenForEvent: listenForEvent, voteBulk: voteBulk}
+}
