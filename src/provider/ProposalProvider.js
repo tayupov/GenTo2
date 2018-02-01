fc424dfa4045896a9e8f2edefdaf0d9e1f095d90
Michael Franz Ettlinger <ettlinger@campus.tu-berlin.de>
2018-02-01 18:25:21 +0100

    Added radix to parseInt for proposal

--- src/provider/ProposalProvider.js
+++ src/provider/ProposalProvider.js
@@ -9,18 +9,18 @@ const mapProposal =  (proposalNumber, proposalArray, proposalStatistics) => {
   return {
       proposalNumber: proposalNumber,
       recipient:proposalArray[0],
-      amount:parseInt(proposalArray[1]),
+       amount:parseInt(proposalArray[1], 10),
       name:proposalArray[2],
       description:proposalArray[3],
       //proposalDeadline:proposalArray[4], // Deprecated
-      finished:parseInt(proposalArray[5]),
+      finished:parseInt(proposalArray[5], 10),
       proposalPassed:proposalArray[6],
-      passedPercent: parseInt(proposalArray[7]), // Deprecated
+      passedPercent: parseInt(proposalArray[7], 10), // Deprecated
       fieldOfWork: proposalArray[8],
-      dividend:parseInt(proposalArray[9]),
-      approve:parseInt(proposalStatistics[0]),
-      disapprove:parseInt(proposalStatistics[1]),
-      percent:parseInt(proposalStatistics[2]),
+      dividend:parseInt(proposalArray[9], 10),
+      approve:parseInt(proposalStatistics[0], 10),
+      disapprove:parseInt(proposalStatistics[1], 10),
+      percent:parseInt(proposalStatistics[2], 10),
       proposalStartTime: parseInt(proposalStatistics[3], 10),
       proposalDeadline:parseInt(proposalStatistics[4], 10),
       currentTime:parseInt(proposalStatistics[5], 10)
