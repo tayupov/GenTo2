3fd83d20cdce1e8b8cd7ab311cb5fc1e694548f5
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2018-02-03 21:50:17 +0100

    cleaned commit

--- solidity/contracts/GentoDao.sol
+++ solidity/contracts/GentoDao.sol
@@ -15,8 +15,6 @@ contract GentoDao is DaoWithDelegation {
     uint8 partner = 25;
 
     event Claimed(string claimType, address beneficiary, uint amount);
-    event Balance(uint balance);
-    event TokenPrice(uint tokenPrice);
 
     function GentoDao(uint256 _maxAmountToRaiseInICO,
     string _symbol,
@@ -28,12 +26,6 @@ contract GentoDao is DaoWithDelegation {
     uint256 _saleEnd,
     bool _dev) DaoWithDelegation(_maxAmountToRaiseInICO, _symbol, _name, _buyPriceStart, _buyPriceEnd, _saleStart, _saleEnd, _dev) public {
         descriptionHash = _descriptionHash;
-    }
-
-    function getTokenPrice() public constant returns (uint tokenPrice) {
-        tokenPrice = this.balance / totalSupply;
-        NumberLogger("TokenPrice", tokenPrice);
-        return tokenPrice;
     }
 
     function claimPayout(uint proposalNumber) public daoActive returns (uint amount) {
@@ -49,14 +41,12 @@ contract GentoDao is DaoWithDelegation {
         return proposal.amount;
     }
 
-    function claimDividend(uint proposalNumber) public onlyShareholders {
-        Proposal storage proposal = proposals[proposalNumber];
-        require(dividends[msg.sender] > 0 && proposal.claimed[msg.sender] == false);
-
+    function claimDividend() public onlyShareholders {
+        require(dividends[msg.sender] > 0);
+        uint dividend = dividends[msg.sender];
         dividends[msg.sender] = 0;
-        require(msg.sender.send(dividends[msg.sender]));
+        require(msg.sender.send(dividend));
 
-        proposal.claimed[msg.sender] = true;
         Claimed("dividend", msg.sender, dividends[msg.sender]);
     }
 
@@ -116,10 +106,8 @@ contract GentoDao is DaoWithDelegation {
     }
 
     function distributeDividend(uint dividend) {
-        uint tokenPrice = getTokenPrice();
-        TokenPrice(tokenPrice);
         for (uint i = 0; i < shareholders.length; ++i) {
-            uint shareholderDividend = ((balances[shareholders[i]] * tokenPrice) * dividend) / 100;
+            uint shareholderDividend = (dividend * balances[shareholders[i]] * (10 ** 3)) / totalSupply / (10 ** 3);
             dividends[shareholders[i]] += shareholderDividend;
         }
     }
