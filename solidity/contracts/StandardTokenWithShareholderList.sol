99d46265db8e32709071a4c9ecc5112370709b28
Christian M <mischok.christian@gmail.com>
2018-02-07 18:04:54 +0100

    inserted new dev methods in StandardTokenWithShareholder only for testing which require that the dev flag is set, adapted the tests in StandardTokenWithShareholder test class

--- solidity/contracts/StandardTokenWithShareholderList.sol
+++ solidity/contracts/StandardTokenWithShareholderList.sol
@@ -9,11 +9,15 @@ contract StandardTokenWithShareholderList is DevContract, StandardToken{
     string public symbol = "";
     string public name = "";
     address[] public shareholders;
-<<<<<<< HEAD
 
     event NewShareholderList(address[] shareholders);
     event BalanceUpdated(uint oldBalance, uint newBalance, uint balance);
     event TransferSuccess(bool success);
+
+    modifier devFlag {
+        require(active);
+        _;
+    }
 
     function StandardTokenWithShareholderList(string _symbol,
     string _name,
@@ -22,8 +26,17 @@ contract StandardTokenWithShareholderList is DevContract, StandardToken{
         name = _name;
     }
 
-    function sendEtherToWallet(uint val) public payable {
-        msg.sender.send(val);
+    // the dev methods for only testing to invoke private and internal methods
+    function setBalanceDev(address user, uint256 newBalance) public devFlag {
+        setBalance(user, newBalance);
+    }
+
+    function removeShareholderDev(address user) public devFlag {
+        removeShareholder(user);
+    }
+
+    function onBalanceChangeDev(address user, uint256 oldBalance, uint256 newBalance) public devFlag {
+        onBalanceChange(user, oldBalance, newBalance);
     }
 
     function transfer(address dst, uint wad) public returns (bool) {
@@ -64,7 +77,7 @@ contract StandardTokenWithShareholderList is DevContract, StandardToken{
         return balances[userAddress] > 0;
     }
 
-    function removeShareholder(address user) /*internal*/ public {
+    function removeShareholder(address user) internal {
         for (uint i = 0; i < shareholders.length; i++) {
             if (shareholders[i] == user) {
                 delete shareholders[i];
@@ -76,89 +89,14 @@ contract StandardTokenWithShareholderList is DevContract, StandardToken{
         revert();
     }
 
-    function setBalance(address user, uint256 newBalance) /*internal*/ public {
+    function setBalance(address user, uint256 newBalance) internal {
         uint oldBalance = balances[user];
         balances[user] = newBalance;
         BalanceUpdated(oldBalance, newBalance, balances[user]);
         onBalanceChange(user, oldBalance, newBalance);
     }
 
-=======
-
-    event NewShareholderList(address[] shareholders);
-    event BalanceUpdated(uint oldBalance, uint newBalance, uint balance);
-    event TransferSuccess(bool success);
-
-    function StandardTokenWithShareholderList(string _symbol,
-    string _name,
-    bool _dev) public DevContract(_dev) {
-        symbol = _symbol;
-        name = _name;
-    }
-
-    function sendEtherToWallet(uint val) public payable {
-        msg.sender.send(val);
-    }
-
-    function transfer(address dst, uint wad) public returns (bool) {
-        uint oldBalanceDest = balances[dst];
-        uint oldBalanceSender = balances[msg.sender];
-        bool result = BasicToken.transfer(dst, wad);
-        onBalanceChange(msg.sender, oldBalanceSender, balances[msg.sender]);
-        onBalanceChange(dst, oldBalanceDest, balances[dst]);
-        TransferSuccess(result);
-        return result;
-    }
-
-    function transferFrom(address src, address dst, uint wad) public returns (bool) {
-        uint oldBalanceDest = balances[dst];
-        uint oldBalanceSrc = balances[src];
-        bool result = StandardToken.transferFrom(src, dst, wad);
-        onBalanceChange(src, oldBalanceSrc, balances[src]);
-        onBalanceChange(dst, oldBalanceDest, balances[dst]);
-        return result;
-    }
-
-    function getShareholderCount() public constant returns(uint count) {
-        uint length = 0;
-        for (uint i = 0; i < shareholders.length; i++) {
-            // check that the address in the list is not set = 0x0...
-            if (shareholders[i] != address(0)) {
-                length++;
-            }
-        }
-        return length;
-    }
-
-    function getBalance(address user) public returns (uint) {
-        return balances[user];
-    }
-
-    function isShareholder(address userAddress) public returns (bool shareholder) {
-        return balances[userAddress] > 0;
-    }
-
-    function removeShareholder(address user) /*internal*/ public {
-        for (uint i = 0; i < shareholders.length; i++) {
-            if (shareholders[i] == user) {
-                delete shareholders[i];
-                NewShareholderList(shareholders);
-                return;
-            }
-        }
-        //If we did not find the shareholder to be removed we have a programming error
-        revert();
-    }
-
-    function setBalance(address user, uint256 newBalance) /*internal*/ public {
-        uint oldBalance = balances[user];
-        balances[user] = newBalance;
-        BalanceUpdated(oldBalance, newBalance, balances[user]);
-        onBalanceChange(user, oldBalance, newBalance);
-    }
-
->>>>>>> added events in StandartTokenWithShareholderList.sol for testing, modified the getShareholderCounts method, added a new method getBalance, and inserted in every method public modifier because its nessecary for testing
-    function onBalanceChange(address user, uint256 oldBalance, uint256 newBalance) /*private*/ public {
+    function onBalanceChange(address user, uint256 oldBalance, uint256 newBalance) private {
         if (oldBalance == 0 && newBalance != 0) {
             shareholders.push(user);
         } else {
