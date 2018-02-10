6445320eb8e23fbbc80b77608b2c2979c45d4229
Daniel Wenzel <daniel.wenzel@campus.tu-berlin.de>
2018-02-10 17:16:51 +0100

    Added amazing toydata

--- src/routes/DAO/components/Description.js
+++ src/routes/DAO/components/Description.js
@@ -8,7 +8,7 @@ export default class Description extends React.Component {
     return (
       <Segment>
           {!description && <div className="skeleton"></div>}
-          {description && <p><ReactMarkdown source={this.props.description} /></p>}
+          {description && <p><ReactMarkdown source={this.props.description} className="description"/></p>}
         <Divider />
         <p id='ipfs-disclaimer'>Offchained to IPFS</p>
       </Segment>
