738d6b4d76ea6d8edcab442f654c2f28b75a675c
Michael Franz Ettlinger <ettlinger@campus.tu-berlin.de>
2018-02-10 13:44:58 +0100

    Added markdown to dao detaisl

--- src/routes/DAO/components/Description.js
+++ src/routes/DAO/components/Description.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Segment, Divider } from 'semantic-ui-react'
+import ReactMarkdown from 'react-markdown'
 
 export default class Description extends React.Component {
   render() {
     return (
       <Segment>
-        {this.props.description || <div className='skeleton'></div>}
+          <p><ReactMarkdown source={this.props.description} /></p>
         <Divider />
         <p id='ipfs-disclaimer'>Offchained to IPFS</p>
       </Segment>
