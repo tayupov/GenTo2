68f9c3463e1c1aaa2f49111af79225a50c3d9271
Paul Benedikt Visser <visser.p.b@googlemail.com>
2018-03-14 14:39:56 +0100

    readme

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -7,21 +7,28 @@ export default () => (
       <h1>About GenTo</h1>
       <Divider section hidden />
       <p>
-        GenTo is a platform that facilitates Initial Coin Offerings and creation/governance of DAOs.
-        It creates user-defined
-        Smart Contracts and automatically deploys them to the blockchain - without the need
-        to write a single line of code!
+        GenTo is a platform that facilitates creating Decentralized Autonomous
+        Organizations (DAOs). It creates user-defined Smart Contracts and
+        automatically deploys them to the blockchain. The platform allows
+        running an Initial Coin Offering for a DAO and implements basic
+        governance mechanisms, that enable token holders to decide over the
+        usage of the organizsation's funds. The application assets are deployed
+        to the InterPlanetary File System. All of this without the need to write
+        a single line of code!
       </p>
       <p>
-        Hiding technical details from the user, we make DAOs accessible for
-        a broader, non-technical audience. GenTo provides an intuitive web-based UI that
-        guides the user through the contract creation process step by step.
+        With the governance mechanisms we make investing in Initial Coin
+        Offerings safer for investors. Hiding technical details from the user,
+        we make creating Decentralized Autonomous Organizations accessible for a
+        broader, non-technical audience. GenTo provides an intuitive web-based
+        UI that guides the user through the contract creation process step by
+        step..
       </p>
       <p>
-          GenTo2 has been developed by a student group at the chair for Information Systems
-          Engineering at Technical University of Berlin in collaboration with venture
-          capitalist and startup advisor Globumbus.
+        GenTo2 has been developed by a student group at the chair for
+        Information Systems Engineering at Technical University of Berlin in
+        collaboration with venture capitalist and startup advisor Globumbus.
       </p>
     </Container>
   </div>
-)
+);
